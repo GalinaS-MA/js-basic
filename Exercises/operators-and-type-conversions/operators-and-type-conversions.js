4f7150c1621e0514edbf69403bea67431924475b
Galyna Saik <gasa@Galynas-MacBook-Pro.local>
2019-10-13 16:45:57 +0300

    HomeWork for data-types, numbers, strings, variables

---
+++ Exercises/operators-and-type-conversions/operators-and-type-conversions.js
@@ -0,0 +1,12 @@
+// 1. Get `100` using `+` operator and numeric value `10`.
+
+let value = 10 + '0';
+console.log(value);
+// 2. You have following string: `Amazon annual revenue of US$232.887 billion` extract revenue and count monthly income.
+
+let string = 'Amazon annual revenue of US$232.887 billion';
+
+let number = parseInt(string);
+console.log(number);
+
+// 3. Declare variable which will contain logs counter. Log few times with updating this value accordingly.
